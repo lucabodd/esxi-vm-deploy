7e8db5d2aefa50fe8cc8b28fa641250073fb102c
Luca Bodini <k3rnelp4n1c@MacBook-Pro-di-Luca.local>
2020-02-26 23:07:30 +0100

    gathering ESXi info

--- ESXi-vm-deploy.go
+++ ESXi-vm-deploy.go
@@ -4,12 +4,16 @@ import (
 	"flag"
 	"os"
 	"fmt"
+	ansibler "github.com/apenella/go-ansible"
+	"bytes"
 )
 
 func main() {
-	fmt.Println("vim-go")
+	//set ansible env vars
+	os.Setenv("ANSIBLE_STDOUT_CALLBACK", "json")
+	os.Setenv("ANSIBLE_HOST_KEY_CHECKING", "False")
 	conf := flag.String("c", "","Specify the configuration file.")
-	vm_name := flag.String("vm-name", "", "Specify virtual machine name")
+	/*vm_name := flag.String("vm-name", "", "Specify virtual machine name")
 	vm_os := flag.String("vm-os", "debian10-64", "Specify virtual machine OS")
 	vm_cpu := flag.Int("cpu", 1, "Specify virtual machine CPU")
 	vm_disk_size := flag.Int("disk-size", 50, "Specify .vmdk size")
@@ -17,10 +21,12 @@ func main() {
 	vm_net := flag.String("vm-net", "", "Specify virtual network")
 	vm_ipv4 := flag.String("ip", "", "Virtual machine IP address")
 	esxi_datastore := flag.String("datastore", "", "Specify Datastore")
+	*/
 	esxi_host := flag.String("esxi", "", "Specify ESXi Host")
+	/*
 	helper_host := flag.String("helper", "", "Specify helper host")
 	ip_automatic_lookup := flag.Bool("ip-automatic-lookup", false, "Specify if script should seek for an unallocated IP address")
-
+	*/
     flag.Parse()
 
 	if *conf != "" {
@@ -29,7 +35,22 @@ func main() {
 		fmt.Println(file)
 	} else {
 		fmt.Println("[*] Configuration file checking other flags")
-
+		ansiblePlaybookConnectionOptions := &ansibler.AnsiblePlaybookConnectionOptions{}
+		ansiblePlaybookOptions := &ansibler.AnsiblePlaybookOptions{
+			Inventory: "dc/auto/inventory",
+			Limit: *esxi_host,
+		}
+		stdout := new(bytes.Buffer)
+		playbook := &ansibler.AnsiblePlaybookCmd{
+			Playbook:          "playbooks/gather-esxi-info.yml",
+			ConnectionOptions: ansiblePlaybookConnectionOptions,
+			Options:           ansiblePlaybookOptions,
+			ExecPrefix:        "",
+			Writer:				stdout,
+		}
+		err := playbook.Run()
+		check(err)
+		fmt.Println(stdout.String())
 	}
 }
 
