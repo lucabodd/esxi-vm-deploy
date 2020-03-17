2efcab207e9893b060c347938ab9f3a991df6a5e
Luca Bodini <lucabodd@gmail.com>
2020-03-17 17:40:52 +0100

    retriving datastores and vmnets from esxi hosts

--- ESXi-vm-deploy.go
+++ ESXi-vm-deploy.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"os"
 	"fmt"
+	"log"
 	ansibler "github.com/apenella/go-ansible"
 	"bytes"
 	"strings"
@@ -14,7 +15,11 @@ func main() {
 	//set ansible env vars
 	os.Setenv("ANSIBLE_STDOUT_CALLBACK", "json")
 	os.Setenv("ANSIBLE_HOST_KEY_CHECKING", "False")
-	conf := flag.String("c", "","Specify the configuration file.")
+
+	//vars
+	var conf string
+	var esxi_host string
+
 	/*vm_name := flag.String("vm-name", "", "Specify virtual machine name")
 	vm_os := flag.String("vm-os", "debian10-64", "Specify virtual machine OS")
 	vm_cpu := flag.Int("cpu", 1, "Specify virtual machine CPU")
@@ -22,25 +27,25 @@ func main() {
 	vm_ram := flag.Int("ram", 16, "Specify RAM size")
 	vm_net := flag.String("vm-net", "", "Specify virtual network")
 	vm_ipv4 := flag.String("ip", "", "Virtual machine IP address")
-	esxi_datastore := flag.String("datastore", "", "Specify Datastore")
 	*/
-	esxi_host := flag.String("esxi", "", "Specify ESXi Host")
+	flag.StringVar(&conf, "c", "", "Specify the configuration file.")
+	flag.StringVar(&esxi_host, "esxi", "", "Specify ESXi Host")
 	/*
 	helper_host := flag.String("helper", "", "Specify helper host")
 	ip_automatic_lookup := flag.Bool("ip-automatic-lookup", false, "Specify if script should seek for an unallocated IP address")
 	*/
     flag.Parse()
 
-	if *conf != "" {
-		file, err := os.Open(*conf)
+	if conf != "" {
+		file, err := os.Open(conf)
 		check(err)
+		//parse configuration files
 		fmt.Println(file)
 	} else {
-		fmt.Println("[*] Configuration file checking other flags")
+		fmt.Println("[*] Configuration file not provided parsing flags")
 		ansiblePlaybookConnectionOptions := &ansibler.AnsiblePlaybookConnectionOptions{}
 		ansiblePlaybookOptions := &ansibler.AnsiblePlaybookOptions{
-			Inventory: "dc/auto/inventory",
-			Limit: *esxi_host,
+			Inventory: esxi_host+",",
 		}
 		stdout := new(bytes.Buffer)
 		playbook := &ansibler.AnsiblePlaybookCmd{
@@ -55,9 +60,12 @@ func main() {
 		json_stdout := strings.Replace(stdout.String(), "=>", "", -1)
 		json_stdout = strings.Replace(json_stdout, json_stdout[len(json_stdout)-23:], "", -1)
 		esxi_vmnet := gjson.Get(json_stdout, "plays.0.tasks.0.hosts.*.stdout_lines")
-		esxy_vmnet := esxi_vmnet.Array()
-		esxy_datastores := gjson.Get(json_stdout, "plays.0.tasks.1.hosts.*.stdout_lines")
-		esxy_datastores := esxy_datastores.Array()
+		vmnets := esxi_vmnet.Array()
+		esxi_datastores := gjson.Get(json_stdout, "plays.0.tasks.1.hosts.*.stdout_lines")
+		datastores := esxi_datastores.Array()
+
+		fmt.Println(vmnets)
+		fmt.Println(datastores)
 
 	}
 }
