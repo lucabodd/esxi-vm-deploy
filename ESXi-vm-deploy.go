0df2e6fef4efe060345ab89e8201a9c2008e82d9
Luca Bodini <lucabodd@gmail.com>
2020-04-18 01:57:56 +0200

    Docs, usage messages and gofmt

--- ESXi-vm-deploy.go
+++ ESXi-vm-deploy.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"flag"
-	"os"
-	"fmt"
-	"log"
-	ansibler "github.com/apenella/go-ansible"
 	"bytes"
-	"strings"
-	"github.com/tidwall/gjson"
-	"bufio"
-	"strconv"
+	"flag"
+	"fmt"
+	ansibler "github.com/apenella/go-ansible"
 	"github.com/schollz/progressbar"
+	"github.com/tidwall/gjson"
+	"log"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -21,7 +19,6 @@ func main() {
 	os.Setenv("ANSIBLE_HOST_KEY_CHECKING", "False")
 
 	//vars
-	var conf string
 	var esxi_host string
 	var vm_name string
 	var vm_os string
@@ -33,156 +30,154 @@ func main() {
 	var vm_disk_size string
 	var helper_host string
 	var help bool
-	reader := bufio.NewReader(os.Stdin)
 
 	//Flag parsing
-	flag.StringVar(&conf, "c", "", "Specify the configuration file.")
-	flag.StringVar(&esxi_host, "esxi", "", "Specify ESXi Host")
+	flag.StringVar(&esxi_host, "esxi", "", "ESXi Hypervisor")
 	flag.StringVar(&vm_name, "vm-name", "", "Specify virtual machine name")
 	flag.StringVar(&vm_os, "vm-os", "debian10-64", "Specify virtual machine OS")
 	flag.StringVar(&vm_ram, "vm-ram", "16", "Specify RAM size")
 	flag.StringVar(&vm_cpu, "vm-cpu", "2", "Specify RAM size")
 	flag.StringVar(&vm_ipv4, "vm-ip", "", "Virtual machine IP address")
-	flag.StringVar(&vm_disk_size, "vm-disk-size", "50", "Virtual machine IP address")
-	flag.StringVar(&helper_host, "helper", "", "Virtual machine IP address")
+	flag.StringVar(&vm_disk_size, "vm-disk-size", "50", "Virtual machine Disk size")
+	flag.StringVar(&helper_host, "helper", "", "BOOTP server address, specified host will provide configurations to booting (PXE) virtual machine")
 	flag.BoolVar(&help, "help", false, "prints this help message")
-    flag.Parse()
-	if help {
+	flag.Parse()
+	if esxi_host == "" || vm_name == "" || vm_ipv4 == "" || helper_host == "" || help {
+		fmt.Println("[-] ERROR: Not enough arguments")
+		fmt.Println("Usage: ESXi-vm-deploy [OPTIONS]")
+		fmt.Println("One ore more required flag has not been prodided.")
+		fmt.Println("Note that using less flag than defined could lead program into errors \nOmit flags only if you are aware of what are you doin'")
 		flag.PrintDefaults()
 		kill("ERR: NOT ENAUGH ARGS")
 	}
 	//end of flag parsing
 
 	/*
-	############################################################################
-	#							VARS COLLECTION 							   #
-	############################################################################
+		############################################################################
+		#							VARS COLLECTION 							   #
+		############################################################################
 	*/
-	//check if configuration file is provided
-	if conf != "" {
-		file, err := os.Open(conf)
-		check(err)
-		//parse configuration files
-		fmt.Println(file)
-	} else {
-		fmt.Println("[*] Configuration file not provided parsing flags")
-		ansiblePlaybookConnectionOptions := &ansibler.AnsiblePlaybookConnectionOptions{}
-		ansiblePlaybookOptions := &ansibler.AnsiblePlaybookOptions{
-			Inventory: esxi_host+",",
-		}
-		stdout := new(bytes.Buffer)
-		playbook := &ansibler.AnsiblePlaybookCmd{
-			Playbook:          "playbooks/esxi-gather-info.yml",
-			ConnectionOptions: ansiblePlaybookConnectionOptions,
-			Options:           ansiblePlaybookOptions,
-			ExecPrefix:        "",
-			Writer:				stdout,
-		}
-		err := playbook.Run()
-		check(err)
-		json_stdout := strings.Replace(stdout.String(), "=>", "", -1)
-		json_stdout = strings.Replace(json_stdout, json_stdout[len(json_stdout)-23:], "", -1)
-		esxi_vmnet := gjson.Get(json_stdout, "plays.0.tasks.0.hosts.*.stdout_lines")
-		vmnets := esxi_vmnet.Array()
-		esxi_datastores := gjson.Get(json_stdout, "plays.0.tasks.1.hosts.*.stdout_lines")
-		datastores := esxi_datastores.Array()
-
-		//VMNET selection
-		if len(vmnets) == 1 {
-			vm_net = vmnets[0].Str
-			log.Println("[+] Only 1 available networks found, selecting "+ vm_net)
-		} else {
-			log.Println("[*] Found more than one virtual network, choose where you want to deploy VM")
-			fmt.Println("Available networks:")
-			for index,element := range vmnets {
-				fmt.Println(index,"-",element)
-			}
-			fmt.Print("Insert ID: ")
-			in, _ := reader.ReadString('\n')
-			id, _ := strconv.Atoi(in)
-			vm_net = vmnets[id].Str
-		}
-		//DATASTORE selection
-		if len(datastores) == 1 {
-			vm_datastore = datastores[0].Str
-			log.Println("[+] Only 1 available datastore found, selecting "+ vm_datastore)
-		} else {
-			log.Println("[*] Found more than one datastore, choose where you want to deploy VM")
-			fmt.Println("Available datastores:")
-			for index,element := range datastores {
-				fmt.Println(index,"-",element)
-			}
-			fmt.Print("Insert ID: ")
-			in, _ := reader.ReadString('\n')
-			id, _ := strconv.Atoi(in)
-			vm_datastore = datastores[id].Str
-		}
-	}
-	/*
-	############################################################################
-	#							VARS COLLECTION END 						   #
-	############################################################################
-	*/
-	/*
-	############################################################################
-	#							VMX DEPLOYMENT  							   #
-	############################################################################
-	*/
-	log.Println("[+] RA passed, deploying .vmx and allocating disk space (thick)")
-	log.Println("[*] Deploying .vmx file")
+	fmt.Println("[*] Configuration file not provided parsing flags")
+	fmt.Println("[*] Gathering ESXi host info")
 	ansiblePlaybookConnectionOptions := &ansibler.AnsiblePlaybookConnectionOptions{}
 	ansiblePlaybookOptions := &ansibler.AnsiblePlaybookOptions{
-		Inventory: esxi_host+",",
-		ExtraVars: map[string]interface{}{
-			"vm_name": vm_name,
-			"vm_os": vm_os,
-			"vm_cpu": vm_cpu,
-			"vm_disk_size": vm_disk_size,
-			"vm_ram": vm_ram,
-			"vm_net": vm_net,
-			"vm_datastore": vm_datastore,
-		},
+		Inventory: esxi_host + ",",
 	}
 	stdout := new(bytes.Buffer)
 	playbook := &ansibler.AnsiblePlaybookCmd{
+		Playbook:          "playbooks/esxi-gather-info.yml",
+		ConnectionOptions: ansiblePlaybookConnectionOptions,
+		Options:           ansiblePlaybookOptions,
+		ExecPrefix:        "",
+		Writer:            stdout,
+	}
+	err := playbook.Run()
+	check(err)
+	json_stdout := strings.Replace(stdout.String(), "=>", "", -1)
+	json_stdout = strings.Replace(json_stdout, json_stdout[len(json_stdout)-23:], "", -1)
+	esxi_vmnet := gjson.Get(json_stdout, "plays.0.tasks.0.hosts.*.stdout_lines")
+	vmnets := esxi_vmnet.Array()
+	esxi_datastores := gjson.Get(json_stdout, "plays.0.tasks.1.hosts.*.stdout_lines")
+	datastores := esxi_datastores.Array()
+
+	//VMNET selection
+	if len(vmnets) == 1 {
+		vm_net = vmnets[0].Str
+		log.Println("[+] Only 1 available networks found, selecting " + vm_net)
+	} else {
+		log.Println("[*] Found more than one virtual network, choose where you want to deploy VM")
+		fmt.Println("Available networks:")
+		for index, element := range vmnets {
+			fmt.Println(index, "-", element)
+		}
+		fmt.Print("Insert ID: ")
+		var id int
+		_, err := fmt.Scanf("%d", &id)
+		check(err)
+		vm_net = vmnets[id].Str
+
+	}
+	//DATASTORE selection
+	if len(datastores) == 1 {
+		vm_datastore = datastores[0].Str
+		log.Println("[+] Only 1 available datastore found, selecting " + vm_datastore)
+	} else {
+		log.Println("[*] Found more than one datastore, choose where you want to deploy VM")
+		fmt.Println("Available datastores:")
+		for index, element := range datastores {
+			fmt.Println(index, "-", element)
+		}
+		fmt.Print("Insert ID: ")
+		var id int
+		_, err := fmt.Scanf("%d", &id)
+		check(err)
+		vm_datastore = datastores[id].Str
+	}
+	/*
+		############################################################################
+		#							VARS COLLECTION END 						   #
+		############################################################################
+	*/
+	/*
+		############################################################################
+		#							VMX DEPLOYMENT  							   #
+		############################################################################
+	*/
+	log.Println("[+] RA passed, deploying .vmx and allocating disk space (thick)")
+	log.Println("[*] Deploying .vmx file")
+	ansiblePlaybookConnectionOptions = &ansibler.AnsiblePlaybookConnectionOptions{}
+	ansiblePlaybookOptions = &ansibler.AnsiblePlaybookOptions{
+		Inventory: esxi_host + ",",
+		ExtraVars: map[string]interface{}{
+			"vm_name":      vm_name,
+			"vm_os":        vm_os,
+			"vm_cpu":       vm_cpu,
+			"vm_disk_size": vm_disk_size,
+			"vm_ram":       vm_ram,
+			"vm_net":       vm_net,
+			"vm_datastore": vm_datastore,
+		},
+	}
+	stdout = new(bytes.Buffer)
+	playbook = &ansibler.AnsiblePlaybookCmd{
 		Playbook:          "playbooks/esxi-deploy-vmx.yml",
 		ConnectionOptions: ansiblePlaybookConnectionOptions,
 		Options:           ansiblePlaybookOptions,
 		ExecPrefix:        "",
-		Writer:				stdout,
+		Writer:            stdout,
 	}
-	err := playbook.Run()
+	err = playbook.Run()
 	check(err)
 	log.Println("[+] Virtual machine created. disk initialization completed")
 	log.Println("[*] Retrieveing VM mac address")
 	log.Println("[*] Retrieveing Assigned VM Hypervisor ID")
-	json_stdout := strings.Replace(stdout.String(), "=>", "", -1)
+	json_stdout = strings.Replace(stdout.String(), "=>", "", -1)
 	json_stdout = strings.Replace(json_stdout, json_stdout[len(json_stdout)-24:], "", -1)
 	mac_stdout := gjson.Get(json_stdout, "plays.0.tasks.4.hosts.*.stdout")
 	vm_mac_addr := mac_stdout.String()
 	id_stdout := gjson.Get(json_stdout, "plays.0.tasks.6.hosts.*.stdout")
 	vm_id := id_stdout.String()
-	log.Println("[+] Got physical address: "+vm_mac_addr)
-	log.Println("[+] Got VMID: "+vm_id)
+	log.Println("[+] Got physical address: " + vm_mac_addr)
+	log.Println("[+] Got VMID: " + vm_id)
 	/*
-	############################################################################
-	#							VMX DEPLOYMENT - END						   #
-	############################################################################
+		############################################################################
+		#							VMX DEPLOYMENT - END						   #
+		############################################################################
 	*/
 	/*
-	############################################################################
-	#							PYBOOTD  DEPLOYMENT 						   #
-	############################################################################
+		############################################################################
+		#							PYBOOTD  DEPLOYMENT 						   #
+		############################################################################
 	*/
 	log.Println("[*] uploading BOOTP server")
 	ansiblePlaybookConnectionOptions = &ansibler.AnsiblePlaybookConnectionOptions{}
 	ansiblePlaybookOptions = &ansibler.AnsiblePlaybookOptions{
-		Inventory: helper_host+",",
+		Inventory: helper_host + ",",
 		ExtraVars: map[string]interface{}{
-			"vm_ipv4": vm_ipv4,
+			"vm_ipv4":     vm_ipv4,
 			"vm_mac_addr": vm_mac_addr,
 			"helper_host": helper_host,
-			"vm_name": vm_name,
+			"vm_name":     vm_name,
 		},
 	}
 	stdout = new(bytes.Buffer)
@@ -191,26 +186,26 @@ func main() {
 		ConnectionOptions: ansiblePlaybookConnectionOptions,
 		Options:           ansiblePlaybookOptions,
 		ExecPrefix:        "",
-		Writer:				stdout,
+		Writer:            stdout,
 	}
 	err = playbook.Run()
 	json_stdout = strings.Replace(stdout.String(), "=>", "", -1)
 	json_stdout = strings.Replace(json_stdout, json_stdout[len(json_stdout)-24:], "", -1)
 	log.Println("[+] BOOTP server running")
 	/*
-	############################################################################
-	#							PYBOOTD  DEPLOYMENT END						   #
-	############################################################################
+		############################################################################
+		#							PYBOOTD  DEPLOYMENT END						   #
+		############################################################################
 	*/
 	/*
-	############################################################################
-	#							VM POWERUP     		 						   #
-	############################################################################
+		############################################################################
+		#							VM POWERUP     		 						   #
+		############################################################################
 	*/
 	log.Println("[*] Powering up VM")
 	ansiblePlaybookConnectionOptions = &ansibler.AnsiblePlaybookConnectionOptions{}
 	ansiblePlaybookOptions = &ansibler.AnsiblePlaybookOptions{
-		Inventory: esxi_host+",",
+		Inventory: esxi_host + ",",
 		ExtraVars: map[string]interface{}{
 			"vm_id": vm_id,
 		},
@@ -221,31 +216,32 @@ func main() {
 		ConnectionOptions: ansiblePlaybookConnectionOptions,
 		Options:           ansiblePlaybookOptions,
 		ExecPrefix:        "",
-		Writer:				stdout,
+		Writer:            stdout,
 	}
 	err = playbook.Run()
 	check(err)
-	log.Println("[+] VM "+ vm_id +" powered on as "+ vm_name)
+	log.Println("[+] VM " + vm_id + " powered on as " + vm_name)
 	/*
-	############################################################################
-	#							VM POWERUP END 		 						   #
-	############################################################################
+		############################################################################
+		#							VM POWERUP END 		 						   #
+		############################################################################
 	*/
+	fmt.Println("\n")
 	bar := progressbar.New(100)
-	for i := 0; i < 100; i++ {
-    	bar.Add(1)
-    	time.Sleep(1000 * time.Millisecond)
+	for i := 0; i < 120; i++ {
+		bar.Add(1)
+		time.Sleep(1000 * time.Millisecond)
 	}
 	fmt.Println("\n")
 	/*
-	############################################################################
-	#							FILE CLEANUP 		 						   #
-	############################################################################
+		############################################################################
+		#							FILE CLEANUP 		 						   #
+		############################################################################
 	*/
 	log.Println("[*] Running Helper deplfiles cleanup")
 	ansiblePlaybookConnectionOptions = &ansibler.AnsiblePlaybookConnectionOptions{}
 	ansiblePlaybookOptions = &ansibler.AnsiblePlaybookOptions{
-		Inventory: helper_host+",",
+		Inventory: helper_host + ",",
 	}
 	stdout = new(bytes.Buffer)
 	playbook = &ansibler.AnsiblePlaybookCmd{
@@ -253,15 +249,15 @@ func main() {
 		ConnectionOptions: ansiblePlaybookConnectionOptions,
 		Options:           ansiblePlaybookOptions,
 		ExecPrefix:        "",
-		Writer:				stdout,
+		Writer:            stdout,
 	}
 	err = playbook.Run()
 	check(err)
 	log.Println("[+] Cleanup Completed")
 	/*
-	############################################################################
-	#							VM CLEANUP END 		 						   #
-	############################################################################
+		############################################################################
+		#							VM CLEANUP END 		 						   #
+		############################################################################
 	*/
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # ESXi-vm-deploy
+Offering a golang program to automate vm installation on a ESXi host.
+The program will be asking for some informations, templating a .vmk file, creating a virtual disk and installing operating system via embedded BOOTP server in python.
+Before starting the automatic deployment of virtual machines you will need to tune up your system in order to be able to run program.
+To run this program you will need:
+* go > 1.13
+* ansible >= 2.7
+
+## System Setup
+In order to setup the system running golang program you will need to follow the follofing steps:
+install ansible via package manager (Debian):
+```
+sudo apt-get install ansible
+```
+Install golang via package manager
+```
+sudo apt-get install golang
+```
+OR from sources
+```
+wget https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz
+tar -xvf go1.13.3.linux-amd64.tar.gz
+mv go /usr/local
+```
+Install ESXi-vm-deploy
+```
+go get github.com/lucabodd/ESXi-vm-deploy
+go install github.com/lucabodd/ESXi-vm-deploy
+```
+In some cases you might need to export this environment vars
+```
+echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
+```
+
+## Usage
+Usage is described running the program with the help flag.
+Before running the program, please consider the following checklist:
+* "helper" host and "vm-ip" must be in the same target subnet (unless you are able to broadcast UDP packets :67,:68 outside the helper's server)
+* one helper can deploy one vm per time, if you want to deploy more than one VM at once, you'll need more helpers (or wait :'))
+
+```
+Usage: ESXi-vm-deploy [OPTIONS]
+One ore more required flag has not been prodided.
+Note that using less flag than defined could lead program into errors
+Omit flags only if you are aware of what are you doin'
+  -esxi string
+        Specify ESXi Host
+  -help
+        prints this help message
+  -helper string
+        BOOTP server address, specified host will provide configurations to booting (PXE) virtual machine
+  -vm-cpu string
+        Specify RAM size (default "2")
+  -vm-disk-size string
+        Virtual machine Disk size (default "50")
+  -vm-ip string
+        Virtual machine IP address
+  -vm-name string
+        Specify virtual machine name
+  -vm-os string
+        Specify virtual machine OS (default "debian10-64")
+  -vm-ram string
+        Specify RAM size (default "16")
+```
