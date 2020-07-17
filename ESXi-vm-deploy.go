89504a508c55b3703e013cb2de79cd6390bffcc6
Luca Bodini <lucabodd@gmail.com>
2020-07-17 11:30:43 +0200

    removing "pause" block from helper deplfile playbook and increasing progressbar timer

--- ESXi-vm-deploy.go
+++ ESXi-vm-deploy.go
@@ -262,7 +262,7 @@ func main() {
 	bar := progressbar.Default(100)
 	for i := 0; i < 100; i++ {
 		bar.Add(1)
-		time.Sleep(1200 * time.Millisecond)
+		time.Sleep(1500 * time.Millisecond)
 	}
 	fmt.Println("\n")
 	/*
